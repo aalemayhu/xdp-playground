a05637827cb1ed24d2d9bee5c48714b74d768e3d
Alexander Alemayhu <alexander@alemayhu.com>
2017-04-11 01:04:54 +0200

    experiment: fix compiler errors in test_run
    Still not working, but no compiler errors. Just warnings
    left.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- experiment/xdp_hello/test_run.c
+++ experiment/xdp_hello/test_run.c
@@ -1,17 +1,26 @@
+#include <linux/bpf.h>
+#include <string.h>
+
+// from kernel tree
+#define BPF_LOG_BUF_SIZE 65536
+
 int main(void)
 {
+	static char log_buf[BPF_LOG_BUF_SIZE];
 	const char *file = "xdp_hello.o";
 	struct bpf_object *obj;
 	int err, fd;
+
+	memset(log_buf, 0, sizeof(log_buf));
 
 	err = bpf_prog_load(file, BPF_PROG_TYPE_XDP, &obj, &fd);
 
 	if (err) {
 		// TODO: give user feedback here.
-		return;
+		return -1;
 	}
 
-	err = bpf_prog_test_run(fd, ...);
+	//err = bpf_prog_test_run(fd, ...);
 
 	bpf_object__close(obj);
 
