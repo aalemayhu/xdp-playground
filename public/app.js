7e70aeeaaeac0f2de76c4489e5c0e1eb8df5059a
Alexander Alemayhu <alexander@alemayhu.com>
2017-04-12 11:18:07 +0200

    public: use variable for title
    This should make it the same everywhere.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- public/app.js
+++ public/app.js
@@ -20,6 +20,8 @@ let Challenges = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 
 let controller = app.controller('MainController', ['$scope', '$http', '$sce',
   function ($scope, $http, $sce) {
 
+  $scope.title = "XDP Playground";
+
   $scope.Challenges = Challenges;
 
 var LoadTask = function(task) {
