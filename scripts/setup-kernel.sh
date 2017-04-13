bffce0555b8f6d741872d96eebd83956141d2535
Alexander Alemayhu <alexander@alemayhu.com>
2017-04-13 18:38:50 +0200

    scripts: extend download to install kernel
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/setup-kernel.sh
+++ scripts/setup-kernel.sh
@@ -2,10 +2,17 @@
 
 branch=xdp-playground.zip
 kernel_dir=~/linux
+build_helper=/vagrant/scripts/linux-dev
 
 if [ ! -d $kernel_dir ]; then
   wget https://github.com/scanf/linux/archive/$branch
   unzip $branch -d $kernel_dir
+  cp `ls -1 /boot/config*|head -n1` $kernel_dir/.config
 else
   echo $kernel_dir already there, skipping
 fi
+
+cd $kernel_dir
+$build_helper build
+$build_helper install
+systemctl reboot
