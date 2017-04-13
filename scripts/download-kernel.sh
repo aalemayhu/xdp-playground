096b8c7f12ba5747a9fb92da86512bcf70de3a80
Alexander Alemayhu <alexander@alemayhu.com>
2017-04-13 18:35:26 +0200

    scripts: setup kernel to be as expected by application
    The path should be right now, but untested.
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/download-kernel.sh
+++ scripts/download-kernel.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 branch=xdp-playground.zip
-wget https://github.com/scanf/linux/archive/$branch
-unzip $branch
+kernel_dir=~/linux
+
+if [ ! -d $kernel_dir ]; then
+  wget https://github.com/scanf/linux/archive/$branch
+  unzip $branch -d $kernel_dir
+else
+  echo $kernel_dir already there, skipping
+fi
