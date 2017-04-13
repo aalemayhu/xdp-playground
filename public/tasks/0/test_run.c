907f83da140b15394bb834ab56b729809fbf77a8
Alexander Alemayhu <alexander@alemayhu.com>
2017-04-13 15:28:33 +0200

    public/tasks0: try to reuse Makefile from kernel
    linux/tools/testing/selftests/bpf
    
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- public/tasks/0/test_run.c
+++ public/tasks/0/test_run.c
@@ -1,26 +1,37 @@
 #include <linux/bpf.h>
 #include <string.h>
+#include <error.h>
+#include <unistd.h>
+#include <linux/bpf.h>
+#include <linux/err.h>
+#include <bpf/bpf.h>
+#include <bpf/libbpf.h>
+#include "bpf_util.h"
 
-// from kernel tree
+// This file is based on examples from the kernel tree.
 #define BPF_LOG_BUF_SIZE 65536
 
 int main(void)
 {
 	static char log_buf[BPF_LOG_BUF_SIZE];
-	const char *file = "0.o";
+	const char *file = "./0.o";
 	struct bpf_object *obj;
-	int err, fd;
+	u32 duration, retval;
+	int err, prog_fd;
 
 	memset(log_buf, 0, sizeof(log_buf));
 
-	err = bpf_prog_load(file, BPF_PROG_TYPE_XDP, &obj, &fd);
-
-	if (err) {
-		// TODO: give user feedback here.
+	err = bpf_prog_load(file, BPF_PROG_TYPE_XDP, &obj, &prog_fd);
+	if (err) {// TODO: how do we get log_buf info?
+		fprintf(stderr, "Failed to load prog\n%s\n", log_buf);
 		return -1;
 	}
 
-	//err = bpf_prog_test_run(fd, ...);
+	// Why not bpf_prog_test_run_xdp(...) ?
+	err = bpf_prog_test_run(prog_fd, 3, NULL, 0, NULL, NULL, &retval, &duration);
+	CHECK(err || errno || retval, "", "err %d errno %d retval %d duration %d\n",
+
+	      err, errno, retval, duration);
 
 	bpf_object__close(obj);
 
