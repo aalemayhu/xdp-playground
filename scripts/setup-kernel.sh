7125409ce66fc217828475446af3ebf07ddae2b6
Alexander Alemayhu <alexander@alemayhu.com>
2017-04-13 21:47:45 +0200

    scripts: less output for
    less spam from wget
    
    Reference: cilium d6b13222f897 (Use -nv when using wget to download stuff, 2017-03-25)
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/setup-kernel.sh
+++ scripts/setup-kernel.sh
@@ -8,7 +8,7 @@ kernel_dir=~/linux
 build_helper=/vagrant/scripts/linux-dev
 
 if [ ! -d $kernel_dir ]; then
-  wget https://github.com/scanf/linux/archive/$branch
+  wget -nv https://github.com/scanf/linux/archive/$branch
   unzip $branch -d $kernel_dir
   cp `ls -1 /boot/config*|head -n1` $kernel_dir/.config
   cd $kernel_dir
