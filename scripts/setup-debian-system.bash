45687286158eab5950dbd3f43fcea61a6473b028
Alexander Alemayhu <alexander@alemayhu.com>
2017-06-18 23:24:26 +0200

    scripts: remove tester user and systemd from default debian setup
    Signed-off-by: Alexander Alemayhu <alexander@alemayhu.com>

--- scripts/setup-debian-system.bash
+++ scripts/setup-debian-system.bash
@@ -3,14 +3,9 @@
 apt-get install -y npm nginx
 make setup
 
-if ! id -u tester; then
-  make tester
-fi
-
 if [[ "`pwd`" != "/srv/app" ]]; then
  ln -s `pwd` /srv/app
 fi
-make systemd
 
 cp nginx.conf.sigil /etc/nginx/sites-enabled/default
 
